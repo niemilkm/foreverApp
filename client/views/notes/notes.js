65177c6a40f4ee8f356b8d6a1320da15d755ed8f
Kevin NIemiller <niemilkm@gmail.com>
2014-01-01 21:45:10 -0500

    Able to add notes and display notes
    working on filtering by folder or section

--- client/views/notes/notes.js
+++ client/views/notes/notes.js
@@ -1,9 +1,40 @@
 
   Template.showNotes.eachNote = function () {
+  	var folderSelected = document.getElementById("selectFolder")
+  	var sectionSelected = document.getElementById("selectSection")
+  	if (folderSelected == "folderAll")
+  	{
+  		folderSelected = 1;
+  	}
+  	if (sectionSelected == "sectionAll")
+  	{
+  		sectionSelected = 1;
+  	}
     return Notes.find({}, {sort: {folder:1, section:1, note:1}});
   };
 
-  Template.showFolders.eachFolder = function ()
+  Template.showFoldersMenu.eachFolder = function ()
   {
   	return Notes.find({}, {sort: {folder:1}});
+  };
+
+ Template.showFoldersSelector.eachFolder = function ()
+  {
+  	return Notes.find({}, {sort: {folder:1}});
+  };
+
+  Template.showSectionsSelector.eachSection = function ()
+  {
+  	return Notes.find({}, {sort: {section:1}});
+  };
+
+  Template.newNote.events =
+  {
+  	'click input.addItem': function()
+  	{
+  		var newFolder = document.getElementById("addFolder").value.trim();
+  		var newSection = document.getElementById("addSection").value.trim();
+  		var newNote = document.getElementById("addNote").value.trim();
+  		Notes.insert({folder:newFolder, section:newSection, note:newNote });
+  	}
   };
