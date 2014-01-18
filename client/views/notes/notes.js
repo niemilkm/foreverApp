a87a797700d7a4e801f910c311a43623157f478b
Kevin NIemiller <niemilkm@gmail.com>
2014-01-18 16:58:39 -0500

    Folder and Section select work to update the table
    Removed all code and put it back in as I was getting stuff to work.
    Will now get the add new note functionality to work.

--- client/views/notes/notes.js
+++ client/views/notes/notes.js
@@ -1,88 +1,50 @@
 
-  Template.showNotes.eachNote = function () {
-  	var folderSelected = document.getElementById("selectFolder");
-  	var sectionSelected = document.getElementById("selectSection");
-  	if ((folderSelected == "folderAll" || folderSelected == null) && (sectionSelected == "sectionAll" || sectionSelected == null))
-  	{
-  		return Notes.find({}, {sort: {folder:1, section:1, note:1}});
-  	}
-  	else if (folderSelected != "folderAll" && sectionSelected != "sectionAll")
-  	{
-  		return Notes.find({folder: folderSelected, section: sectionSelected}, {sort: {folder:1, section:1, note:1}});
-  	}
-  	else if (folderSelected != "folderAll")
-  	{
-  		return Notes.find({folder: folderSelected}, {sort: {folder:1, section:1, note:1}});
-  	}
-  	else
-  	{
-  		return Notes.find({section: sectionSelected}, {sort: {folder:1, section:1, note:1}});
-  	}
-    
-  };
 
-  Template.showFoldersMenu.eachFolder = function ()
-  {
-  	return FoldersDB.find({}, {sort: {folderName:1}});
-  };
 
  Template.showFoldersSelector.eachFolder = function ()
   {
   	return FoldersDB.find({}, {sort: {folderName:1}});
   };
 
-  Template.showSectionsSelector.eachSection = function ()
+  Template.getSectionOptions.eachSection = function ()
   {
-  	//return SectionsDB.find({}, {sort: {sectionName:1}});
     return SectionsDB.find({folderName: Session.get("folderSelected")}, {sectionName:1});
   };
 
-  Template.newNote.events =
-  {
-  	'click input.addItem': function()
-  	{
-  		var newFolder = document.getElementById("addFolder").value.trim();
-  		var newSection = document.getElementById("addSection").value.trim();
-  		var newNote = document.getElementById("addNote").value.trim();
-      var folderNameCount = FoldersDB.find( {folderName: newFolder}).count();
-      if (folderNameCount > 0)
-      {
-        var sectionNameCount = SectionsDB.find( {folderName: newFolder} && {sectionName: newSection}).count();
-        if (sectionNameCount > 0)
-        {
-        }
-        else
-        {
-          SectionsDB.insert({folderName: newFolder, sectionName:newSection});
-        }
-      }
-      else
-      {
-        FoldersDB.insert({folderName:newFolder});
-        SectionsDB.insert({folderName: newFolder, sectionName:newSection});
-      }
-      
-      Notes.insert({folder:newFolder, section:newSection, note:newNote });
-  	}
+  Template.showNotes.eachNote = function () {
+
+    if (Session.get("folderSelected") == "folderAll" || Session.get("folderSelected") == null)
+    {
+      return Notes.find({}, {sort: {folder:1, section:1, note:1}});
+    }
+    else if (Session.get("sectionSelected") == "sectionAll")
+    {
+      return Notes.find({folder: Session.get("folderSelected")}, {sort: {folder:1, section:1, note:1}});
+    }
+    else
+    {
+      return Notes.find({folder: Session.get("folderSelected"), section: Session.get("sectionSelected")}, {sort: {folder:1, section:1, note:1}});
+    }
+    
   };
 
-  Template.showFoldersSelector.events =
+  Template.noteBody.events =
   {
-  	'change #selectFolder': function(event)
+  	'change #selectFolder': function(evt)
   	{
   		//var folderSelected = document.getElementById("selectFolder");
 	  	//var sectionSelected = document.getElementById("selectSection");
 	  	//Session.set("folderSelected", evt.currentTarget.value);
-      Session.set("folderSelected", Document.getElementById("selectFolder"));
-      var testSessionVariable = Session.get("folderSelected");
-  	}
+      Session.set("folderSelected", evt.currentTarget.value);
+  	},
+
+    'change #selectSection': function(evt)
+    {
+      //var folderSelected = document.getElementById("selectFolder");
+      //var sectionSelected = document.getElementById("selectSection");
+      //Session.set("folderSelected", evt.currentTarget.value);
+      Session.set("sectionSelected", evt.currentTarget.value);
+    }
+
   };
-
-  Template.getSectionOptions.helpers = function()
-  {
-    return SectionsDB.find({folderName: Session.get("folderSelected")}, {sectionName:1});
-
-  }
-
-
 
